bc7a961edda100bcb3608e271b9d3d5d504eaf1f
HowToDisappear <60031509+HowToDisappear@users.noreply.github.com>
2021-07-13 14:34:00 +0300

    upd example

--- example_access.py
+++ example_access.py
@@ -16,25 +16,10 @@ async def foo(pool):
                 await asyncio.sleep(2)
 
 
-async def bar(pool):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as curs:
-            while True:
-                await curs.execute("""
-                SELECT AVG( last_price ) AS BTCUSDT_avg_kucoin
-                FROM crypto
-                WHERE symbol = 'BTCUSDT' AND exchange = 'kucoin';
-                """)
-                avg = await curs.fetchone()
-                print(f"KuCoin BTCUSDT avg: {round(avg[0], 2)}")
-                await asyncio.sleep(2)
-
-
 async def main():
     async with aiopg.create_pool(dsn=f"dbname=monitr host=127.0.0.1") as pool:
         task1 = asyncio.create_task(foo(pool))
-        task2 = asyncio.create_task(bar(pool))
-        await asyncio.gather(task1, task2)
+        await asyncio.gather(task1)
 
 
 if __name__ == '__main__':
